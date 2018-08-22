1670a49af39558a1c900755546b61775da3bc14e
VenkateshLatchireddi <venkatesh.l@way2online.co.in>
2018-08-22 15:07:44 +0530

    added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.use(bodyParser({limit: '50mb'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
